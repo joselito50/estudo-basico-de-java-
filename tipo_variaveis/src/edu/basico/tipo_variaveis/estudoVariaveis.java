7628162c27c4e26100483b3a184d41a2f727449f
joselito50 <z.elito21@hotmail.com>
2023-03-12 18:58:28 -0300

    ok

--- tipo_variaveis/src/edu/basico/tipo_variaveis/estudoVariaveis.java
+++ tipo_variaveis/src/edu/basico/tipo_variaveis/estudoVariaveis.java
@@ -1,5 +1,17 @@
 package edu.basico.tipo_variaveis;
 
 public class estudoVariaveis {
+    /*
+     * São oito tipos de variaveis primitiva do jav que são
+     * INT, BYTE, SHORT, FLOAT, DOUBLE, BOOLEAN E CHAR 
+     * Esses tipos nao são consederados objetos, e portanto representa valores brutos.
+     * elas são armazenados diretamente na pilha de memoria (memory stack)
+     */
+    public static void main(String[] args) {
+        byte idade = 30; // tipo 'byte' 1 byte na memoria vai de -128 ate 127
+        short ano = 2023; // tipo 'short' 2byte na memoria vai de -32768 ate 32767
+        int distancia = 567900900; // tipo 'inteiro' 4 byte na memorai
+        long quantidadeEstrela = 5699; // tipo 'long' 8 byte na memoria
+    }
     
 }
